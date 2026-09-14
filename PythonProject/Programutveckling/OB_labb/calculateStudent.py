4a50bad89207fdbad3aea78d6b1616e89721d256
luckyyylukeeeeeee <lhyden@kth.se>
2026-09-14 20:09:55 +0200

    Initial commit

--- PythonProject/Programutveckling/OB_labb/calculateStudent.py
+++ PythonProject/Programutveckling/OB_labb/calculateStudent.py
@@ -1,138 +1,75 @@
 from studentClassFile import MediaStudent
 from coursesClassFile import Course
-from database import get_students, get_courses, get_registrations
-
+from queryingSqlite import get_students, get_courses, get_registrations
 
 def create_student_objects():
     """Skapar MediaStudent-objekt från databasen."""
-
     student_object_list = []
-
     for student in get_students():
-
         student_object = MediaStudent(
             student_id=student["student_id"],
-            name=student["name"]
-        )
-
+            name=student["name"])
         student_object_list.append(student_object)
-
     return student_object_list
-
 
 def create_course_objects():
     """Skapar Course-objekt från databasen."""
-
     course_object_list = []
-
     for course in get_courses():
-
         course_object = Course(
             course_id=course["course_id"],
             course_code=course["course_code"],
             course_name=course["course_name"],
-            hp=course["hp"]
-        )
-
+            hp=course["hp"])
         course_object_list.append(course_object)
-
     return course_object_list
-
 
 def add_courses_to_students(student_objects, course_objects):
     """Lägger till studenternas klarade kurser."""
-
     registrations = get_registrations()
-
     for registration in registrations:
-
         student_id = registration["student_id"]
         course_id = registration["course_id"]
-
         for student in student_objects:
-
             if student.get_student_id() == student_id:
-
                 for course in course_objects:
-
                     if course.get_course_id() == course_id:
-
-                        student.add_course(course)
-
+                       student.add_course(course)
 
 def calculate_hp_per_student(student_objects):
     """Beräknar hur många högskolepoäng varje student har klarat."""
-
     all_students_points = {}
-
     for student in student_objects:
-
         total_hp = 0
-
         for course in student.get_courses():
             total_hp += course.get_hp()
-
         all_students_points[student.get_name()] = total_hp
-
     return all_students_points
-
 
 def calculate_total_course_points(course_objects):
     """Beräknar det totala antalet högskolepoäng."""
-
     total_amount_of_course_points = 0
-
     for course in course_objects:
         total_amount_of_course_points += course.get_hp()
-
     return total_amount_of_course_points
 
 
 def main():
     """Kör programmet."""
-
-    # Skapar student- och kursobjekt från databasen.
-    student_objects = create_student_objects()
-    course_objects = create_course_objects()
-
-    # Lägger studenternas kurser i deras MediaStudent-objekt.
-    add_courses_to_students(
-        student_objects,
-        course_objects
-    )
-
-    # Beräknar totalt antal obligatoriska poäng.
-    tot_course_points = calculate_total_course_points(
-        course_objects
-    )
-
-    # Beräknar varje students poäng.
-    st_points = calculate_hp_per_student(
-        student_objects
-    )
-
+    student_objects = create_student_objects()   # Skapar student- och kursobjekt från databasen.
+    course_objects = create_course_objects()     # Lägger studenternas kurser i deras MediaStudent-objekt.
+    add_courses_to_students(student_objects,course_objects)
+    tot_course_points = calculate_total_course_points(course_objects)  # Beräknar totalt antal obligatoriska poäng.
+    st_points = calculate_hp_per_student(student_objects)     # Beräknar varje students poäng.
     total_courses_cleared = {}
-
     # Beräknar procentandel klarade poäng.
     for each_student in st_points:
-
-        points = (
-            st_points[each_student] /
-            tot_course_points
-        ) * 100
-
+        points = (st_points[each_student] / tot_course_points) * 100
         total_courses_cleared[each_student] = f"{points:.1f}%"
-
     print(f"Antal obligatoriska poäng: {tot_course_points}")
     print("-" * 32)
-
     for student in total_courses_cleared:
-
-        print(
-            f"{student} har klarat "
-            f"{total_courses_cleared[student]}"
-        )
-
+        print(f"{student} har klarat "f"{total_courses_cleared[student]}")
 
 if __name__ == "__main__":
     main()
--- PythonProject/Programutveckling/OB_labb/queryingSqlite.py
+++ PythonProject/Programutveckling/OB_labb/queryingSqlite.py
@@ -5,67 +5,27 @@ def get_students():
     """Hämtar studentinformation från databasen."""
     con = sqlite3.connect("medieteknik.sqlite")
     cur = con.cursor()
-
-    students_rows = cur.execute(
-        "SELECT * FROM Student"
-    )
-
-    students = [
-        {
-            "student_id": row[0],
-            "name": row[1]
-        }
-        for row in students_rows
-    ]
-
+    students_rows = cur.execute("SELECT * FROM Student")
+    students = [{"student_id": row[0],"name": row[1]}for row in students_rows]
     con.close()
-
     return students
 
 
 def get_courses():
     """Hämtar kursinformation från databasen."""
-
     con = sqlite3.connect("medieteknik.sqlite")
     cur = con.cursor()
-
-    courses_rows = cur.execute(
-        "SELECT * FROM Kurs"
-    )
-
-    courses = [
-        {
-            "course_id": row[0],
-            "course_code": row[1],
-            "course_name": row[2],
-            "hp": row[3]
-        }
-        for row in courses_rows
-    ]
-
+    courses_rows = cur.execute("SELECT * FROM Kurs")
+    courses = [{"course_id": row[0], "course_code": row[1],"course_name": row[2],"hp": row[3]} for row in courses_rows]
     con.close()
-
     return courses
 
 
 def get_registrations():
     """Hämtar information om vilka kurser studenter klarat."""
-
     con = sqlite3.connect("medieteknik.sqlite")
     cur = con.cursor()
-
-    registrations_rows = cur.execute(
-        "SELECT student_id, kurs_id FROM Kursregistrering"
-    )
-
-    registrations = [
-        {
-            "student_id": row[0],
-            "course_id": row[1]
-        }
-        for row in registrations_rows
-    ]
-
+    registrations_rows = cur.execute("SELECT student_id, kurs_id FROM Kursregistrering")
+    registrations = [{"student_id": row[0],"course_id": row[1]}for row in registrations_rows]
     con.close()
-
     return registrations
